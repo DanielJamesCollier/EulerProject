8cd099fd0fe1d006ead994116c2146d830acbc87
Daniel Collier <daniel.collier@hotmail.co.uk>
2017-02-11 12:04:39 +0000

    remove unused code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cassert>
 
-// c++ 14 needed
-
 /*
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
@@ -64,6 +62,6 @@ int main() {
     std::cout << "problemTwo: " << problemThree() << std::endl;
     std::cout << "-----------------------------" << std::endl;
 
-    problemThree();
+
     return 0;
 }
